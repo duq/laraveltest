af1cc17bad08994e343c6c68f93fcaad429d37f8
duq <ptd.duke@gmail.com>
2017-11-04 20:13:24 +0100

    laravel testing

---
+++ Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+
+/*
+    public function fileUpload(Request $request)
+    {
+
+        //dd($request);
+
+        $request->file('file')->store('filesdir');
+
+        return back();
+
+    }*/
+}
